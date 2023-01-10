b175d7f432ff132256eff9b7ff7618a503abaab9
Nife16 <100443715+Nife16@users.noreply.github.com>
2023-01-10 10:52:35 -0600

    my first commit, has some homework

---
+++ Practice.java
@@ -0,0 +1,22 @@
+public class Practice {
+    
+    public static void main(String[] args) {
+
+
+        if(true || false && false || false) {
+            System.out.println("Purchase it");
+        } else {
+            System.out.println("wtf mate");
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ homework/Practice.java
@@ -0,0 +1,28 @@
+package homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Practice {
+    
+    public static void main(String[] args) {
+
+
+        String word = "word";
+        String another = "word";
+
+        word = word.concat(another);
+        System.out.println(word);
+
+        
+
+
+
+
+
+
+
+    }
+
+
+}
